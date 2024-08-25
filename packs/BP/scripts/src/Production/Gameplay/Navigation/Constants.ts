14a8547bc906efea3d6a13ede3fa63af975b1baf
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-08-25 17:34:10 +1200

    Warp Compass

---
+++ packs/BP/scripts/src/Production/Gameplay/Navigation/Constants.ts
@@ -0,0 +1,6 @@
+import { Vector2, Vector3 } from '@minecraft/server'
+
+export const Spawn: { location: Vector3; rotation: Vector2 } = {
+  location: { x: 0.5, y: 158.0, z: -235.5 },
+  rotation: { x: 0, y: 90 }
+}
--- packs/BP/scripts/src/Production/Gameplay/Navigation/Warps.ts
+++ packs/BP/scripts/src/Production/Gameplay/Navigation/Warps.ts
@@ -1,18 +1,55 @@
 import { system, world } from '@minecraft/server'
 import { ActionFormData } from '@minecraft/server-ui'
+import { Gamemodes } from '../Gamemodes'
+import { Spawn } from './Constants'
 
 world.beforeEvents.itemUse.subscribe(data => {
   if (data.itemStack.typeId === 'minecraft:compass') {
     const WarpsForm = new ActionFormData()
       .title('§8WARPS')
+      .button('§8Spawn')
       .button('§8KitPvP')
       .button('§8Adventure')
+      .button('§8Arcade')
       .button('§8Skywars')
       .button('§8Bridge')
       .button('§8Spleef')
 
     system.run(() => {
-      WarpsForm.show(data.source).then()
+      WarpsForm.show(data.source).then(result => {
+        if (!result.canceled && result.selection !== undefined) {
+          switch (result.selection) {
+            case 0:
+              // Spawn
+              data.source.tryTeleport(Spawn.location, { rotation: Spawn.rotation })
+              break
+            case 1:
+              // KitPvP
+              data.source.tryTeleport(Gamemodes[0].location, { rotation: Gamemodes[0].rotation })
+              break
+            case 2:
+              // Adventure
+              data.source.tryTeleport(Gamemodes[1].location, { rotation: Gamemodes[1].rotation })
+              break
+            case 3:
+              // Arcade
+              data.source.tryTeleport(Gamemodes[2].location, { rotation: Gamemodes[2].rotation })
+              break
+            case 4:
+              // Skywars
+              data.source.tryTeleport(Gamemodes[3].location, { rotation: Gamemodes[3].rotation })
+              break
+            case 5:
+              // Bridge
+              data.source.tryTeleport(Gamemodes[4].location, { rotation: Gamemodes[4].rotation })
+              break
+            case 6:
+              // Spleef
+              data.source.tryTeleport(Gamemodes[5].location, { rotation: Gamemodes[5].rotation })
+              break
+          }
+        }
+      })
     })
   }
 })
